673ef562cad9e9fa535c9fa2da1c044de35bcb20
DevM7mdAli <DevM7mdAli@outlook.com>
2024-02-23 23:36:59 +0300

    milestone 2 finished

--- lib/main.dart
+++ lib/main.dart
@@ -18,47 +18,67 @@ void main() {
   ));
 }
 
-class ImagesPage extends StatelessWidget{
+class ImagesPage extends StatefulWidget {
+  const ImagesPage({super.key});
+
   @override
-  Widget build(BuildContext context){
-    return Container(
-      child:  Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          const Text("Try Again",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-          ),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                flex: 1,
-                child: TextButton(
-                  onPressed: () => {},
-                  child: Image.asset("Images/image-1.png", 
-                                      width: 180,
-                    ),
-                ),
-                ),
-
-
-              Expanded(
-                flex: 1,
-                child: TextButton(
-                  onPressed: () => {},
-                  child: Image.asset("Images/image-1.png", 
-                                      width: 180,
-                    ),
-                ),
-                ),
-          ],),
-        ],
-      ),
-    );
-  }
+  State<ImagesPage> createState() => _ImagesPageState();
 }
+
+class _ImagesPageState extends State<ImagesPage> {
+                  
+                  int x = 1 , y = 1;
+                  @override
+                  Widget build(BuildContext context){
+
+                    return Container(
+                      child:  Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const Text("Try Again",
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                          ),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                    if(x == 9){x = 1;}
+                                    else{x = x + 1; }
+                                    });
+                                  },
+                                  child: Image.asset("Images/image-$x.png", 
+                                                      width: 180,
+                                    ),
+                                ),
+                                ),
+
+
+                              Expanded(
+                                flex: 1,
+                                child: TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                    if(y == 9){y = 1;}
+                                    else{y = y + 1; }
+                                    });
+                                  },
+                                  child: Image.asset("Images/image-$y.png", 
+                                                      width: 180,
+                                    ),
+                                ),
+                                ),
+                          ],),
+                        ],
+                      ),
+                    );
+                  }
+                }
