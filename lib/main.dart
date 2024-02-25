3f7382385ae5884d94f38a7e828c5c9dabdc3fbf
DevM7mdAli <DevM7mdAli@outlook.com>
2024-02-25 10:25:13 +0300

    new update adding counters , ratio of win and reset button

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,20 @@ class ImagesPage extends StatefulWidget {
 
 class _ImagesPageState extends State<ImagesPage> {
   int x = 1, y = 2;
+  int wonCounter = 0, numberOfTry = 0;
+
+  void cheker() {
+    if (x == y) {
+      wonCounter++;
+    } else {
+      numberOfTry++;
+    }
+  }
 
   void randomNumber() {
     x = Random().nextInt(8) + 1;
     y = Random().nextInt(8) + 1;
+    cheker();
   }
 
   @override
@@ -40,13 +50,44 @@ class _ImagesPageState extends State<ImagesPage> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Text(
-          x == y ? 'You Won' : "Try Again",
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            color: x == y ? Colors.white : Colors.red,
-          ),
+        Column(
+          children: [
+            Text(
+              x == y ? 'You Won' : "Try Again",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: x == y ? Colors.white : Colors.red,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Number of wins: $wonCounter",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+            Text(
+              "Number of try: $numberOfTry",
+              style: const TextStyle(
+                color: Colors.red,
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              "Ratio Of wining: ${(numberOfTry == 0 || wonCounter == 0 ? "0" : (wonCounter.toDouble() / numberOfTry.toDouble() * 100).toStringAsFixed(2))}%",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ],
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -80,6 +121,29 @@ class _ImagesPageState extends State<ImagesPage> {
               ),
             ),
           ],
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.pinkAccent),
+          onPressed: (numberOfTry == 0 && wonCounter == 0)
+              ? null
+              : () {
+                  setState(() {
+                    numberOfTry = 0;
+                    wonCounter = 0;
+                  });
+                },
+          child: Text(
+            numberOfTry == 0 && wonCounter == 0
+                ? "You Must Play First"
+                : "Reset All Try And Wins",
+            style: TextStyle(
+                fontSize: 20,
+                color: numberOfTry == 0 && wonCounter == 0
+                    ? Colors.black
+                    : Colors.white),
+          ),
         ),
       ],
     );
